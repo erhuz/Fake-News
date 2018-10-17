7aa8d0411096c35a04d8cbc61e06b8ef43a7cb68
Benjamin Fransson <benjamin@cafcon.se>
2018-10-17 13:08:44 +0200

    Added components

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare (strict_types = 1);
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
@@ -7,12 +7,24 @@ declare(strict_types=1);
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="css/bootswatch.css">
-        <title></title>
-    </head>
-    <body>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootswatch.css">
+    <title>Fake news</title>
+</head>
+
+<body>
+    <?php require_once('components/nav.php'); ?>
+
+    <!-- CONTENT BEGIN -->
+    <main>
+
+    </main>
+    <!-- CONTENT END -->
+
+    <?php require_once('components/footer.php'); ?>
+</body>
+
 </html>
