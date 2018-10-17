5cc7907db84800896b62a900e4afbf7c61e09b88
Benjamin Fransson <benjamin@cafcon.se>
2018-10-17 11:37:45 +0200

    Added bootswatch css framework

--- index.php
+++ index.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="css/bootswatch.css">
         <title></title>
     </head>
     <body>
