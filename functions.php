efaf574e5195555ecbc9c756479df78b02146e42
Benjamin Fransson <benjamin@cafcon.se>
2018-10-18 15:41:48 +0200

    Added classes: AuthorManager & NewsManager

--- functions.php
+++ functions.php
@@ -1,5 +1,29 @@
 <?php
 
-class Author{
-    
+class AuthorManager{
+    private $database;
+
+    public function __construct()
+    {
+        $database = new ConnectToDatabase;
+    }
+
+    public function create(string $name, string $email, string $password)
+    {
+        
+    }
+}
+
+class NewsManager{
+    private $database;
+
+    public function __construct()
+    {
+        $database = new ConnectToDatabase;
+    }
+
+    public function create(string $title, string $content, string $author)
+    {
+        
+    }
 }
