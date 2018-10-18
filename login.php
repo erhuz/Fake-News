577c2559158f94d235ac803b09573fe3328a1f56
Benjamin Fransson <benjamin@cafcon.se>
2018-10-18 15:44:03 +0200

    Added hidden input

--- login.php
+++ login.php
@@ -37,6 +37,7 @@ $title = 'Become an author';
                                 <input type="password" name="pwd" id="pwd"><br>
                                 <label for="pwd">Confirm Password</label><br>
                                 <input type="password" name="pwd2" id="pwd2"><br>
+                                <input type="hidden" name="action" value="register">
                                 <input class="mt-3 btn btn-primary" type="submit" value="Register">
                             </form>
                         </div>
@@ -53,6 +54,7 @@ $title = 'Become an author';
                                 <input type="email" name="email" id="email"><br>
                                 <label for="pwd">Password</label><br>
                                 <input type="password" name="pwd" id="pwd"><br>
+                                <input type="hidden" name="action" value="login">
                                 <input class="mt-3 btn btn-primary" type="submit" value="Login">
                             </form>
                         </div>
