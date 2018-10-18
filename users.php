29bf09c4a44f0c5d3adf570fc6d7ba0a13704427
Benjamin Fransson <benjamin@cafcon.se>
2018-10-18 15:44:09 +0200

    Added form handling

--- users.php
+++ users.php
@@ -2,3 +2,20 @@
     if(!isset($_POST['action'])){
         header('location: /');
     }
+
+    $db = new ConnectToDatabase;
+    $author = new AuthorManager;
+
+    switch ($_POST['action']) {
+        case 'register':
+
+            break;
+        
+        case 'login':
+            # code...
+            break;
+
+        default:
+            # code...
+            break;
+    }
