84d5333c51fffdacf843b2fe00a5e4d74132be7b
Benjamin Fransson <benjamin@cafcon.se>
2018-10-19 18:02:16 +0200

    Formatted files for consistency

--- login.php
+++ login.php
@@ -3,14 +3,13 @@ declare (strict_types = 1);
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
+
+// Page setup
 session_start();
-
 require_once 'classes/db.php';
-
 $title = 'Login / Register';
 
-
-// Require HTML
+/* REQUIRE HTML */
 
 require_once 'components/head.php';
 
