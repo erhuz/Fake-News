47c195f9b8d5938c672fe6348cbfaf214d0c1292
Benjamin Fransson <benjamin@cafcon.se>
2018-10-20 00:53:32 +0200

    Split accounts.php into login.php & register.php

---
+++ processing/login.php
@@ -0,0 +1,57 @@
+<?php
+    session_start();
+
+    require_once $_SERVER['DOCUMENT_ROOT'].'/database/db.php';
+
+    // IF
+    if(!isset($_POST['email']) || !isset($_POST['pwd'])){
+
+        // Prepare message
+        $tmp_msg['content'] = 'Something went wrong! Please try again.';
+        $tmp_msg['type'] = 'danger';
+
+        // Set message
+        $_SESSION['messages'][] = $tmp_msg;
+        header('location: /');
+    }
+
+    $db = new ConnectToDatabase;
+
+    $query = "SELECT * FROM authors WHERE email=:email;";
+    $params = [':email' => $_POST['email']];
+    
+    if($result = $db->getData($query, $params)){
+        // If passwords doesn't match
+        if($result[0]['password'] !== hash('sha256', $_POST['pwd'])){
+
+            // Prepare message
+            $tmp_msg['title'] = 'Login Failed';
+            $tmp_msg['content'] = 'Email or Password is invalid';
+            $tmp_msg['type'] = 'danger';
+    
+            // Set message
+            $_SESSION['messages'][] = $tmp_msg;
+
+            // Redirect back to login/register
+            header('location: /login.php');
+            exit;
+        }
+
+        // Prepare message
+        $tmp_msg['title'] = 'Login Successful';
+        $tmp_msg['content'] = 'You\'re now logged in! Enjoy!';
+        $tmp_msg['type'] = 'success';
+
+        // Set message
+        $_SESSION['messages'][] = $tmp_msg;
+
+        // Set login session variables
+        $_SESSION['user']['id'] = $result[0]['id'];
+        $_SESSION['user']['name'] = $result[0]['name'];
+        $_SESSION['user']['email'] = $result[0]['email'];
+        $_SESSION['user']['date'] = $result[0]['date'];
+
+        // Redirect back to login/register
+        header('location: /login.php');
+        exit;
+    }
---
+++ processing/register.php
@@ -0,0 +1,73 @@
+<?php
+    session_start();
+    require_once $_SERVER['DOCUMENT_ROOT'].'/database/db.php';
+
+    if(!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['pwd']) || !isset($_POST['pwd2'])){
+
+        // Prepare message
+        $tmp_msg['content'] = 'Something went wrong! Please try again.';
+        $tmp_msg['type'] = 'danger';
+
+        // Set message
+        $_SESSION['messages'][] = $tmp_msg;
+
+        // Redirect back to login/register
+        header('location: /');
+        exit;
+    }
+
+    $db = new ConnectToDatabase;
+
+
+    $query = "SELECT * FROM authors WHERE email=:email;";
+    $params = [':email' => $_POST['email']];
+    
+    if($db->getData($query, $params)){
+
+        // Prepare message
+        $tmp_msg['title'] = 'Registration Failed';
+        $tmp_msg['content'] = 'An account is already associated with that email, try another one.';
+        $tmp_msg['type'] = 'danger';
+
+        // Set message
+        $_SESSION['messages'][] = $tmp_msg;
+
+        // Redirect back to login/register
+        header('location: /login.php');
+        exit;
+    }
+    
+    $query = 'INSERT INTO authors(name, email, password) VALUES(:name, :email, :password);';
+    $params = [
+        ':name' => $_POST['name'],
+        ':email' => $_POST['email'],
+        ':password' => hash('sha256', $_POST['pwd'])
+    ];
+    
+    if($db->setData($query, $params)){
+
+        // Prepare message
+        $tmp_msg['title'] = 'Registration Complete!';
+        $tmp_msg['content'] = 'Registration successful, you can now log in!';
+        $tmp_msg['type'] = 'success';
+
+        // Set message
+        $_SESSION['messages'][] = $tmp_msg;
+
+        // Redirect back to login/register
+        header('location: /login.php');
+        exit;
+    }else{
+
+        // Prepare message
+        $tmp_msg['title'] = 'Registration Failed';
+        $tmp_msg['content'] = 'Failed to store provided information';
+        $tmp_msg['type'] = 'danger';
+
+        // Set message
+        $_SESSION['messages'][] = $tmp_msg;
+
+        // Redirect back to login/register
+        header('location: /login.php');
+        exit;
+    }
