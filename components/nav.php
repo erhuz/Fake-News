8ef40c970697bb57e92e3daccd8f8e923d28cf3b
Benjamin Fransson <benjamin@cafcon.se>
2018-10-20 00:52:50 +0200

    Added conditional author navbar

--- components/nav.php
+++ components/nav.php
@@ -1,3 +1,35 @@
+<?php if(isset($_SESSION['user'])): ?>
+<!-- NAVBAR BEGIN -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">Fake News</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/">News</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/authors.php">Authors</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/login.php">Create an article</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/processing/logout.php">Logout</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+<!-- NAVBAR END -->
+<?php else: ?>
 <!-- NAVBAR BEGIN -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="#">Fake News</a>
@@ -25,3 +57,4 @@
     </div>
 </nav>
 <!-- NAVBAR END -->
+<?php endif; ?>
