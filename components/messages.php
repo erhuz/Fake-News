7fc80511f04381ee0bb942dad0e1d89581abedeb
Benjamin Fransson <benjamin@cafcon.se>
2018-10-19 18:03:23 +0200

    Added message support

---
+++ components/messages.php
@@ -0,0 +1,73 @@
+<?php
+
+if(isset($_SESSION['messages'])):
+
+    $messages = $_SESSION['messages'];
+
+?>
+<div class="container mt-4"> <!-- Messages container -->
+    <?php foreach($messages as $mess): ?>  
+        <div class="row"> <!-- Message row -->
+            <?php if($mess === 'secondary'): ?>
+            <div div class="col-12 alert alert-dismissible alert-secondary">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <?php if(isset($mess['title'])): ?>
+                    <h4><?= $mess['title']; ?></h4>
+                <?php endif; ?>
+                <p><?= $mess['content']; ?></p>
+            </div>
+
+            <?php elseif($mess === 'success'): ?>
+            <div class="col-12 alert alert-dismissible alert-success">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <?php if(isset($mess['title'])): ?>
+                    <h4><?= $mess['title']; ?></h4>
+                <?php endif; ?>
+                <p><?= $mess['content']; ?></p>
+            </div>
+
+            <?php elseif($mess === 'info'): ?>
+            <div class="col-12 alert alert-dismissible alert-info">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <?php if(isset($mess['title'])): ?>
+                    <h4><?= $mess['title']; ?></h4>
+                <?php endif; ?>
+                <p><?= $mess['content']; ?></p>
+            </div>
+
+            <?php elseif($mess === 'warning'): ?>
+            <div class="col-12 alert alert-dismissible alert-warning">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <?php if(isset($mess['title'])): ?>
+                    <h4><?= $mess['title']; ?></h4>
+                <?php endif; ?>
+                <p><?= $mess['content']; ?></p>
+            </div>
+
+            <?php elseif($mess === 'dangar'): ?>
+            <div class="col-12 alert alert-dismissible alert-danger">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <?php if(isset($mess['title'])): ?>
+                    <h4><?= $mess['title']; ?></h4>
+                <?php endif; ?>
+                <p><?= $mess['content']; ?></p>
+            </div>
+
+            <?php else: ?>
+            <div class="col-12 alert alert-dismissible alert-primary">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <?php if(isset($mess['title'])): ?>
+                    <h4><?= $mess['title']; ?></h4>
+                <?php endif; ?>
+                <p><?= $mess['content']; ?></p>
+            </div>
+
+            <?php endif; ?>
+        </div> <!-- /Message row -->
+    <?php endforeach; ?>
+</div> <!-- /Messages container -->
+
+<?php
+$_SESSION['messages'] = [];
+unset($_SESSION['messages']);   // Remove messages after being listed.
+endif; 
