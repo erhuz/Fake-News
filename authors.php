70f20dd2e9bd36c766a3a15f1fa6bda9aea333ca
Benjamin Fransson <benjamin@cafcon.se>
2018-10-19 17:52:41 +0200

    Added Sessions to visitable pages

--- authors.php
+++ authors.php
@@ -3,6 +3,8 @@ declare (strict_types = 1);
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
+session_start();
+
 require_once 'classes/db.php';
 
 $title = 'Authors';
--- login.php
+++ login.php
@@ -3,7 +3,9 @@ declare (strict_types = 1);
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
-require_once('classes/db.php');
+session_start();
+
+require_once 'classes/db.php';
 
 $title = 'Login / Register';
 
