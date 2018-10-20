36146ead7e8959be67b6893574270ec02affca61
Benjamin Fransson <benjamin@cafcon.se>
2018-10-20 11:23:47 +0200

    Added comments

--- processing/logout.php
+++ processing/logout.php
@@ -1,7 +1,10 @@
 <?php 
+// Start session (otherwhise we can't access the session vars)
 session_start();
 
+// Remove the user session
 unset($_SESSION['user']);
 
-
+// Redirect back to the start page
 header('location: /');
+exit;
