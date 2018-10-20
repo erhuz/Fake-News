4846a1cea4547270436db04771eaa30c143f22dd
Benjamin Fransson <benjamin@cafcon.se>
2018-10-21 00:56:19 +0200

    Added new page

---
+++ my_news.php
@@ -0,0 +1,23 @@
+<?php
+declare (strict_types = 1);
+// This is the file where you can keep your HTML markup. We should always try to
+// keep us much logic out of the HTML as possible. Put the PHP logic in the top
+// of the files containing HTML or even better; in another PHP file altogether.
+
+// Page setup
+session_start();
+require_once $_SERVER['DOCUMENT_ROOT'].'/database/db.php';
+$title = 'My News';
+
+
+/* REQUIRE HTML */
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/components/head.php';
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/components/nav.php';
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/components/messages.php';
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/content/my_news.php'; // Content
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/components/footer.php';
