917a0e285839bb9d48d854f394831f6379dc212a
Benjamin Fransson <benjamin@cafcon.se>
2018-10-22 22:01:52 +0200

    Updated link

--- components/nav.php
+++ components/nav.php
@@ -1,7 +1,7 @@
 <?php if(isset($_SESSION['user'])): ?>
 <!-- NAVBAR BEGIN -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">Fake News</a>
+    <a class="navbar-brand" href="/">Fake News</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01"
         aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -32,7 +32,7 @@
 <?php else: ?>
 <!-- NAVBAR BEGIN -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">Fake News</a>
+    <a class="navbar-brand" href="/">Fake News</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01"
         aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
