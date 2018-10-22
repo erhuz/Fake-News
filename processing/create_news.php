c82c28585bbdb2bafac577a5deb578ac91e4ff16
Benjamin Fransson <benjamin@cafcon.se>
2018-10-22 15:54:03 +0200

    Fixed: header location

--- processing/create_news.php
+++ processing/create_news.php
@@ -13,11 +13,38 @@ if(!isset($_POST['title']) || !isset($_POST['content'])){
     setMessage('Input went missing! Please try again.', 'Woops!', 'danger');
 
     // Redirect back to my_news
-    header('/my_news.php');
+    header('location: /my_news.php');
     exit;
 }
 
 // Sanitize user input
 foreach($_POST as $key => $value){
     $_POST[$key] = strip_tags(htmlentities($value));
+}
+
+// Set insert query & parameters
+$query = 'INSERT INTO news(title, content, likes) VALUES(:title, :content, :likes);';
+$params = [
+    ':title' => $_POST['title'],
+    ':content' => $_POST['content'],
+    ':likes' => rand(0, 40)
+];
+
+// If data successfully inserted
+if($db->setData($query, $params)){
+
+    // Set message
+    setMessage('Created a news article, go check it out!', 'Successfully created an article!', 'success');
+
+    // Redirect back to my news
+    header('location: /my_news.php');
+    exit;
+}else{
+    
+    // Set message
+    setMessage('Failed to create an article.', 'Could not create article!', 'danger');
+
+    // Redirect back to my news
+    header('location: /my_news.php');
+    exit;
 }
