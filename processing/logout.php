45bf5b740e2d630796a37d4367eb2ad20629bc75
Benjamin Fransson <benjamin@cafcon.se>
2018-10-22 15:55:20 +0200

    Added logout message

--- processing/logout.php
+++ processing/logout.php
@@ -1,9 +1,13 @@
 <?php 
 // Start session (otherwhise we can't access the session vars)
 session_start();
+require_once $_SERVER['DOCUMENT_ROOT'].'/functions.php';
 
 // Remove the user session
 unset($_SESSION['user']);
+
+// Set logout message
+setMessage('You\'re now logged out.');
 
 // Redirect back to the start page
 header('location: /');
