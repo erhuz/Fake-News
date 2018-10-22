7a3b493fed9bda3f982c866627e28761b629fd5d
Benjamin Fransson <benjamin@cafcon.se>
2018-10-22 22:02:41 +0200

    Added individual view for specified authors

--- content/author.php
+++ content/author.php
@@ -1,4 +1,37 @@
-<?php 
-declare (strict_types = 1);
+<main>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col">
+                <h1><?=$title?></h1>
+            </div>
+        </div>
+        <div class="row">
 
-// TODO: Get given author w/ his/hers articles
+        <!-- put news here -->
+            <?php foreach ($results as $result): ?>
+                <div class="col-md-12 col-lg-6">
+                    <div class="card border-primary mb-3">
+
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col"><h4><?= $result['name']; ?></h4></div>
+                                <div class="col d-flex justify-content-end"><?= $result['date']; ?></div>
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                <?= $result['title']; ?>
+                            </h4>
+                            <p class="card-text">
+                                <?= $result['content']; ?>
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            <?php endforeach;?>
+
+        </div>
+    </div>
+</main>
